29277cf378100027a2f2e5c99a81189c43f19fd7
Other <Other@TRVs-MacBook-Air.local>
2019-06-04 17:57:52 -0500

    css changes

--- game.js
+++ game.js
@@ -15,21 +15,35 @@ $(document).ready(function () {
     ////////
     $("#tablecontainer").on("click", function () {
 
-        var randNumber = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
 
-        $("#tablecontainer").html(randNumber);
+        // console.log(randNumber);
 
-        console.log(randNumber);
+        // ////////    
 
-        ////////    
+        // $("#totalscore").html(randNumber);
 
-        $("#totalscore").html(randNumber);
-
-        $("#scoreLabel").html(randNumber);
+        // // $("#scoreLabel").html("Total: " + randNumber);
 
     });
 
+    $(".crystal-image").on("click", function () {
 
+        // We'll then increment the counter each time by the randomly selected number.
+        // If the number is 11, then users can win. (11 + 11 + 11 + 11 + 11 = 55)
+        // If the number is 10, then there is no way for the user to win. (10 + 10 + 10 + 10 + 10 = 60)
+        counter += increment;
+
+        alert("New score: " + counter);
+
+
+        if (counter === targetNumber) {
+            alert("You win!");
+        }
+
+        else if (counter >= targetNumber) {
+            alert("You lose!!");
+        }
+    });
 
 
 });
