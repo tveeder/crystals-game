41c3a7ddc4c1f9bba4e6663cd9d71b0952d01946
Other <Other@TRVs-MacBook-Air.local>
2019-06-03 21:38:42 -0500

    initial commit of first files

---
+++ game.js
@@ -0,0 +1,24 @@
+
+//  with JQuery
+
+
+alert("this is connected to javascript");
+
+// var score = 0;
+// $(document).ready(function () {
+
+//     $("#randomscore").click(increment);
+
+// }); //end.ready
+
+// function increment() {
+//     score += 1
+
+// }; //end increment
+
+
+
+
+
+
+
