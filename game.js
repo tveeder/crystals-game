21583e5314ccdc6d493dbeb4c26fb373ec6c0dd6
Other <Other@TRVs-MacBook-Air.local>
2019-06-04 23:18:44 -0500

    more changes

--- game.js
+++ game.js
@@ -4,51 +4,92 @@
 
 $(document).ready(function () {
 
-    //////////
     alert("this is connected to javascript");
 
+    var numberOptions = [10, 11];
+    var increment = numberOptions[Math.round(Math.random())];
+    var counter = 0;
+
     $("#randomscore").on("click", function () {
-        var randNumber = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
+        var randNumber = Math.floor(Math.random() * (120 - 1 + 1)) + 1;
         $("#randomscore").html(randNumber);
+        var targetNumber = randNumber;
+        $("#totalscore").html(randNumber);
+        //   return targetNumber;
+
     });
 
-    ////////
-    // $("#tablecontainer").on("click", function () {
-
-    // });
 
     $("#box1").on("click", function () {
 
-        $("#randomscore").html(randNumber);
+        var targetNumber = 30;   // just going to give it a value
+        counter += increment;
+
         alert("New score: " + counter);
 
+        updateScore(counter);
+        scoreCheck(counter, targetNumber);
+
+
     });
+
+    var counter = 0;
+    var numberOptions = [10, 11];
+    var increment = numberOptions[Math.round(Math.random())];
 
     $("#box2").on("click", function () {
-
-
+        counter += increment;
         alert("New score: " + counter);
+
+
     });
+
+    var counter = 0;
+    var numberOptions = [10, 11];
+    var increment = numberOptions[Math.round(Math.random())];
 
     $("#box3").on("click", function () {
-
-
+        counter += increment;
         alert("New score: " + counter);
+
+
     });
+
+    var counter = 0;
+    var numberOptions = [10, 11];
+    var increment = numberOptions[Math.round(Math.random())];
 
     $("#box4").on("click", function () {
-
-
+        counter += increment;
         alert("New score: " + counter);
 
-    });
 
-    // if (counter === targetNumber) {
-    //     alert("You win!");
-    // }
+    })
 
-    // else if (counter >= targetNumber) {
-    //     alert("You lose!!");
-    // }
+    function resetCounts() {
+
+        var targetNumber = 0;
+        var counter = 0;
+        var randNumber = Math.floor(Math.random() * (120 - 1 + 1)) + 1;
+        $("#randomscore").html(randNumber);
+        var targetNumber = randNumber;
+    }
+
+    function scoreCheck(counter, targetNumber) {
+
+        if (counter > targetNumber) {
+            alert("You win!");
+            console.log(targetNumber)
+        }
+        else if (counter < targetNumber) {
+            alert("You lose!!");
+        }
+
+        function updateScore(counter) {
+            $("#totalscore").html(counter);
+
+        }
+
+    }
 
 });
