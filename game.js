41fbb7775cf7311bfe74217d5a02275fde694026
Other <Other@TRVs-MacBook-Air.local>
2019-06-05 18:20:16 -0500

    updates to image

--- game.js
+++ game.js
@@ -9,33 +9,69 @@ $(document).ready(function () {
     var numberOptions = [10, 11];
     var increment = numberOptions[Math.round(Math.random())];
     var counter = 0;
+    var targetNumber = 0;
 
     $("#randomscore").on("click", function () {
         var randNumber = Math.floor(Math.random() * (120 - 1 + 1)) + 1;
         $("#randomscore").html(randNumber);
-        var targetNumber = randNumber;
+        targetNumber = randNumber;
         $("#totalscore").html(randNumber);
+
         //   return targetNumber;
 
     });
 
 
-    $("#box1").on("click", function () {
+    var numberOptions = [10, 5, 3, 7];
 
-        var targetNumber = 30;   // just going to give it a value
-        counter += increment;
+    for (var i = 0; i < numberOptions.length; i++) {
+
+        var imageCrystal = $("<img>");
+        imageCrystal.addClass("crystal-image");
+        imageCrystal.attr("src", "./crystals/images.jpg");
+        imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+        $("#crystals").append(imageCrystal);
+        console.log(imageCrystal);
+    }
+
+    $(".crystal-image").on("click", function () {
+
+        var crystalValue = ($(this).attr("data-crystalvalue"));
+        alert("its working")
+        crystalValue = parseInt(crystalValue);
+        counter += crystalValue;
+
+
 
         alert("New score: " + counter);
 
-        updateScore(counter);
-        scoreCheck(counter, targetNumber);
+        if (counter === targetNumber) {
+            alert("You win!");
+        }
+
+        else if (counter >= targetNumber) {
+            alert("You lose!!");
+        }
+
+    });
+
+    $("#box1").on("click", function () {
+
+        targetNumber = 30;   // just going to give it a value
+        counter += increment;
+
+        $("#totalscore").html(counter);
+        alert("New score: " + counter);
+
+        // updateScore(counter);
+        // scoreCheck(counter, targetNumber);
 
 
     });
 
-    var counter = 0;
-    var numberOptions = [10, 11];
-    var increment = numberOptions[Math.round(Math.random())];
+
+
+
 
     $("#box2").on("click", function () {
         counter += increment;
@@ -87,8 +123,12 @@ $(document).ready(function () {
 
         function updateScore(counter) {
             $("#totalscore").html(counter);
-
+            console.log(counter)
         }
+
+
+
+
 
     }
 
